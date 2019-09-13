97a87a0190b541b96b710f2e8ad2a9dcde07c628
LittleBenXi <15210009859@163.com>
2019-09-13 14:00:58 +0800

    midau

--- run.py
+++ run.py
@@ -17,21 +17,26 @@ def weixin():
         my_nonce = request.args.get('nonce')        # 获取携带的nonce参数
         my_echostr = request.args.get('echostr')         # 获取携带的echostr参数
 
-        token = 'Your token'     # 一定要跟刚刚填写的token一致
+        token = 'LJHljh123'     # 一定要跟刚刚填写的token一致
 
         # 进行字典排序
         data = [token,my_timestamp ,my_nonce ]
         data.sort()
+        print(data)
 
         # 拼接成字符串
         temp = ''.join(data)
+        print(temp,type(temp))
 
         # 进行sha1加密
-        mysignature = hashlib.sha1(temp).hexdigest()
+        s1 = hashlib.sha1()
+        s1.update(temp.encode('utf-8'))
+        mysignature = s1.hexdigest()
+        #mysignature = hashlib.sha1(temp).hexdigest()
 
         # 加密后的字符串可与signature对比，标识该请求来源于微信
         if my_signature == mysignature:
             return my_echostr
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0',port=80)
